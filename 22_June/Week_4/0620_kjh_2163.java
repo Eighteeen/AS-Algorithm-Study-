ec548ee9e668b7ff3cc4a92fec897265126bd7c5
Deepred <deepred.kim@gmail.com>
2022-06-20 13:05:30 +0900

    첨언

--- 22_June/Week_4/0620_kjh_2163.java
+++ 22_June/Week_4/0620_kjh_2163.java
@@ -3,6 +3,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+// 변수명/함수명 등등으로 코드의 의도를 표현하는 식으로 
+// 쉽게 읽히는 코드 지향하고 있음!
+
+// 앞으로도 계속 이런 스타일로 코딩할텐데,
+// 잘 안 읽히는 부분이 보인다면 => 어떻게 바꾸면 더 잘 읽힐 것 같다고 조언해준다면 매우 감사!
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
