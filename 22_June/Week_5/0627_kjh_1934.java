376a18a45cd8b6f7e48bc52f40886a9c92f72304
Deepred <deepred.kim@gmail.com>
2022-06-28 09:39:00 +0900

    0627 풀이

---
+++ 22_June/Week_5/0627_kjh_1934.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder result = new StringBuilder();
+        
+        int testCases = Integer.parseInt(br.readLine());
+        while (testCases-- > 0) {
+            String[] aAndB = br.readLine().split(" ");
+            int a = Integer.parseInt(aAndB[0]);
+            int b = Integer.parseInt(aAndB[1]);
+
+            result.append(getLCM(a, b)).append('\n');
+        }
+
+        System.out.print(result);
+        br.close();
+    }
+
+    public static int getLCM(int a, int b) {
+        return a * b / getGCD(a, b);
+    }
+
+    private static int getGCD(int a, int b) {
+        if (b == 0) return a;
+        return getGCD(b, a % b);
+    }
+}
