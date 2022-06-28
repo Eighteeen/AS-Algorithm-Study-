40171d62853c8930f405c6a2b918fa81a4cc3bcf
jinju7082 <jinju7082@gmail.com>
2022-06-28 17:11:55 +0900

    Create 0627_kjj_1934.py

---
+++ 22_June/Week_5/0627_kjj_1934.py
@@ -0,0 +1,14 @@
+
+n = int(input())
+
+def gcd(a,b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+for i in range(n):
+    a , b = map(int, input().split())
+    g = gcd(a,b)
+    print(int(g*(a/g)*(b/g)))
+
