88e44ad94b1630f9836380323b3f133731e21c39
jinju7082 <jinju7082@gmail.com>
2022-07-03 22:40:57 +0900

    Create 0701_kjh_16435.py

---
+++ 22_July/Week_1/0701_kjh_16435.py
@@ -0,0 +1,12 @@
+fruits, snake_length = map(int, input().split())
+fruit_altitudes = list(map(int, input().split()))
+
+fruit_altitudes.sort()
+## 오 생각해보지 못한 방식이에요 신선해!
+while fruit_altitudes and fruit_altitudes[0] <= snake_length:
+    del fruit_altitudes[0]
+    snake_length += 1
+
+print(snake_length)
+
+## Welcome to python world😊
