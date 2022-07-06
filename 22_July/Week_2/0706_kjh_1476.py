208e6f54046cab3a70ebd25d16993a477d19e277
Deepred <deepred.kim@gmail.com>
2022-07-06 10:52:17 +0900

    0706 문제풀이

---
+++ 22_July/Week_2/0706_kjh_1476.py
@@ -0,0 +1,12 @@
+target_e, target_s, target_m = map(int, input().split())
+
+e, s, m = 1, 1, 1
+year = 1
+
+while e != target_e or s != target_s or m != target_m:
+    e = e%15 + 1
+    s = s%28 + 1
+    m = m%19 + 1
+    year += 1
+
+print(year)
