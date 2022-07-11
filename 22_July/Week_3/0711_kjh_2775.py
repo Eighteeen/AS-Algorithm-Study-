596d41824c69a74750b2eaf422aa425ae6de119e
Deepred <deepred.kim@gmail.com>
2022-07-11 09:37:13 +0900

    0711 문제풀이

---
+++ 22_July/Week_3/0711_kjh_2775.py
@@ -0,0 +1,21 @@
+memoization = [[0 for j in range(15)] for i in range(15)]
+
+def count_residents(floor, unit):
+    if memoization[floor][unit] != 0: return memoization[floor][unit]
+    if floor == 0: return unit
+    
+    count = 0;
+    for i in range(1, unit + 1):
+        count += count_residents(floor - 1, i)
+    
+    memoization[floor][unit] = count
+    return count
+
+
+T = int(input())
+
+for i in range(T):
+    floor = int(input())
+    unit = int(input())
+
+    print(count_residents(floor, unit))
