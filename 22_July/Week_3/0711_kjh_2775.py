a4a209ecf7df170fbd39f1d83acb227da669627d
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2022-07-13 23:53:09 +0900

    Update 0711_kjh_2775.py

--- 22_July/Week_3/0711_kjh_2775.py
+++ 22_July/Week_3/0711_kjh_2775.py
@@ -1,5 +1,5 @@
 memoization = [[0 for j in range(15)] for i in range(15)]
-
+## 이걸 재귀로 푸시다니,,,!
 def count_residents(floor, unit):
     if memoization[floor][unit] != 0: return memoization[floor][unit]
     if floor == 0: return unit
