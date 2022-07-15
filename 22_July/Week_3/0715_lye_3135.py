962cb445d3772f3913f83e99530b1390e5c3fe43
Deepred <deepred.kim@gmail.com>
2022-07-16 00:31:44 +0900

    Update 0715_lye_3135.py

--- 22_July/Week_3/0715_lye_3135.py
+++ 22_July/Week_3/0715_lye_3135.py
@@ -10,8 +10,11 @@ for i in range(0, btnCnt):
 		gap = nowGap
 		targetBtn = nowBtn
 
+## kjh 코드처럼 아예 (gap+1) 값을 기준으로 최소값을 구했다면 if문을 덜 써도 됐었을 것 같습니다
+## 만약 불가피하게 이런 if문을 써야만한다면, +1 할지 말지 여부를 결정하는 Bool 변수를 추가로 사용하는게 직관적이라고 생각합니다. 변수 하나 더 만들어봤자 몇십억 바이트 중 4바이트 더 쓰는거기도하고요
 if targetBtn != 1001:
 	print(gap + 1)
 else:
 	print(gap)
 	
+## 👍
