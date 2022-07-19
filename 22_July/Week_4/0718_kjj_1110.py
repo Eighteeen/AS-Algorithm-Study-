ee04ac354be98dd31aaa27b56e547f4f8f436a5e
Deepred <deepred.kim@gmail.com>
2022-07-19 11:13:20 +0900

    0718 피드백

--- 22_July/Week_4/0718_kjj_1110.py
+++ 22_July/Week_4/0718_kjj_1110.py
@@ -4,7 +4,7 @@ cycle = 0
 
 while 1:
     sum_new = (new // 10) + (new % 10)
-    new_num = ((new % 10) * 10) + (sum_new % 10)
+    new_num = ((new % 10) * 10) + (sum_new % 10) ## new_num 없이 new 변수만 써도 정답으로 잘 나와요 https://www.acmicpc.net/source/46282831
     cycle += 1
 
     if(new_num == n):
