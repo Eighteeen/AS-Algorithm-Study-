273c3e8296800dc45bca55afc60ae09fbdb3931d
Deepred <deepred.kim@gmail.com>
2022-07-20 10:54:34 +0900

    0720 문제풀이

---
+++ 22_July/Week_4/0720_kjh_1205.py
@@ -0,0 +1,13 @@
+rank_count, new_record, rank_limit = map(int, input().split())
+ranks = list(map(int, input().split())) if rank_count > 0 else []
+
+ranks.append(new_record)
+ranks.sort(reverse=True)
+
+rank = ranks.index(new_record) + 1
+
+out_of_rank = len(ranks) > rank_limit and ranks[-1] == new_record
+if out_of_rank:
+    rank = -1
+
+print(rank)
