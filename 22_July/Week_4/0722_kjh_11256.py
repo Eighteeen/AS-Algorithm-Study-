e3f69a03106de31f5a62ae091f176327b84114c0
Deepred <deepred.kim@gmail.com>
2022-07-22 09:26:04 +0900

    0722 문제풀이

---
+++ 22_July/Week_4/0722_kjh_11256.py
@@ -0,0 +1,22 @@
+T = int(input())
+
+for i in range(T):
+  candies, boxes = map(int, input().split())
+
+  box_sizes = []
+  for j in range(boxes):
+    row_size, col_size = map(int, input().split())
+    box_size = row_size * col_size
+    box_sizes.append(box_size)
+  
+  box_sizes.sort(reverse=True)
+
+  boxed_candies = 0
+  used_boxes = 0
+  for box_size in box_sizes:
+    boxed_candies += box_size
+    used_boxes += 1
+    if boxed_candies >= candies:
+      break;
+  
+  print(used_boxes)
