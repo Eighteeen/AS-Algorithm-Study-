f2ac378b855e1aeb5eb76d97d0ba3244d6450471
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2022-07-25 20:42:21 +0900

    Create 0725_lye_1357.py

---
+++ 22_July/Week_5/0725_lye_1357.py
@@ -0,0 +1,9 @@
+def reverseNum(num):
+ rev_list = list(num)
+ rev_list.reverse()
+ res = ''.join(rev_list)
+ return int(res)
+
+num1, num2 = input().split(' ')
+res = reverseNum(num1) + reverseNum(num2)
+print(reverseNum(str(res)))
