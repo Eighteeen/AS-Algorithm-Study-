afda62968f9a3779804d0884955a4e550927e8cc
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2022-08-01 23:17:01 +0900

    Create 0801_lye_1010.py

---
+++ 22_August/Week_1/0801_lye_1010.py
@@ -0,0 +1,12 @@
+caseCnt = int(input())
+
+for i in range(caseCnt):
+ m,n = map(int,input().split(' '))
+ m = m if (m < n-m) else n-m
+ res = 1
+ for j in range(m):
+  res *= n
+  n -= 1
+ for j in range(1, m+1):
+  res = int(res / j)
+ print(res)
