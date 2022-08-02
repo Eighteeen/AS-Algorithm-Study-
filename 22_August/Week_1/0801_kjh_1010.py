9835026df573141fe1534275ec234909f2ba08a8
Deepred <deepred.kim@gmail.com>
2022-08-02 10:25:21 +0900

    Update 0801_kjh_1010.py

--- 22_August/Week_1/0801_kjh_1010.py
+++ 22_August/Week_1/0801_kjh_1010.py
@@ -1,14 +1,7 @@
-def calc_combinations_to_choose_m_from_n(m, n):
-    return factorial(n) // (factorial(m) * factorial(n - m))
-
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
+import math
 
 T = int(input())
 
 for _ in range(T):
-    m, n = map(int, input().split())
-    print(calc_combinations_to_choose_m_from_n(m, n))
+    n, m = map(int, input().split())
+    print(math.comb(m, n))
