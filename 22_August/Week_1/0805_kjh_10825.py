1155ac3901460d7a64674a1a1b660a92293a47e5
Deepred <deepred.kim@gmail.com>
2022-08-05 14:20:17 +0900

    0805 문제풀이

---
+++ 22_August/Week_1/0805_kjh_10825.py
@@ -0,0 +1,19 @@
+N = int(input())
+
+class Student:
+  def __init__(self, name, korean, english, math):
+    self.name = name
+    self.korean = int(korean)
+    self.english = int(english)
+    self.math = int(math)
+
+student_list = []
+
+for _ in range(N):
+  data = input().split()
+  student = Student(*data)
+  student_list.append(student)
+
+student_list.sort(key = lambda s: (-s.korean, s.english, -s.math, s.name))
+for student in student_list:
+  print(student.name)
